aa256313e151e81a7437d3df3dc487697a32c37b
Minseo Kim <gr22nist@gmail.com>
2024-07-18 17:48:07 +0900

    footer component 완료

--- components/footer.js
+++ components/footer.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/footer.module.css';
 
 const Footer = () => {
+
+  const listItemClass = `${styles.list_item} border-2 border-slate-300 hover:opacity-70 ease-in-out`
+
   return (
-    <div className={`${styles.container} bg-slate-200 text-slate-400 mx-auto py-3`}>
-      <div className={`${styles.footer_wrap}`}>
-        <Image src='/img/logo_ci-light.svg' alt='Logo' width={117} height={24} className='mr-2' />
+    <footer className={`${styles.container} w-full bg-slate-200 text-slate-400 py-3`}>
+      <div className={`${styles.footer_wrap} mx-auto`}>
+        <div className='flex items-end gap-4'>
+          <Link href="/" legacyBehavior>  
+            <a>
+              <Image src='/img/logo_ci-light.svg' alt='Logo' width={117} height={24} className='mr-2' />  
+            </a>
+          </Link>
+          <span className='text-xs'>Copyright ©WISE IT All Rights Reserved.</span>
+        </div>
         <div>
             <ul className={`${styles.icon_list} flex flex-row`}>
-                <li className={`${styles.list_item}`}>
+                <li className={listItemClass}>
+                  <Link href="/" legacyBehavior>
+                    <a>
+                      <Image src="/img/link_wise.svg" alt="wise-logo" width={20} height={20}/>
+                    </a>
+                  </Link>
+                </li>
+                <li className={listItemClass}>
                     <a 
                         href='https://github.com'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className='icon_link'>
-                        <Image src='/img/link_wise.svg' alt='wise-logo' width={20} height={20}/>
-                    </a>
-                </li>
-                <li>
-                    <a 
-                        href='https://www.notion.so/ko-kr'
                         target='_blank'
                         rel='noopener noreferrer'
                         className='icon_link border-color-slate-300'>
                         <Image src='/img/link_github.svg' alt='wise-logo' width={20} height={20}/>
                     </a>
                 </li>
-                <li>
+                <li className={listItemClass}>
                     <a 
                         href='https://discord.com'
                         target='_blank'
@@ -39,7 +48,7 @@ const Footer = () => {
             </ul>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
+import * as colors from './tailwind/colors.js';
+
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      bgDark: colors.bgDark,
+      primaryLight: colors.primaryLight,
+      primaryDark: colors.primaryDark,
+      secondaryLight: colors.secondaryLight,
+      secondaryDark: colors.secondaryDark,
+      subLight: colors.subLight,
+      subDark: colors.subDark,
+    },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     },
   },
-  plugins: [],
 };
+export const plugins = [];
