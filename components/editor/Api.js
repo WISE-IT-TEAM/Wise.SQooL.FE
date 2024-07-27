4f7a9e9d034638af5243c875b774a4f55105785b
Minseo Kim <gr22nist@gmail.com>
2024-07-27 23:56:07 +0900

    [수정] 에디터 스타일 변경

---
+++ components/editor/Api.js
@@ -0,0 +1,49 @@
+// component/editor/Api.js
+export const createDatabase = async () => {
+  try {
+    const response = await fetch('https://1e44-121-131-9-81.ngrok-free.app/api/sqool/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ dbname: 'Artist' }),
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      throw new Error('Database creation failed');
+    }
+
+    const data = await response.json();
+    console.log('Database created successfully', data);
+  } catch (error) {
+    console.error('Error creating database:', error);
+  }
+};
+
+export const executeQuery = async (query, setQueryResult) => {
+  console.log('Executing query:', query);
+
+  try {
+    const response = await fetch('https://1e44-121-131-9-81.ngrok-free.app/api/sqool/query', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({ query }),
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.log('Error response text:', errorText);
+      throw new Error('Query execution failed');
+    }
+
+    const result = await response.json();
+    setQueryResult({ columns: result.columns, rows: result.result });
+  } catch (error) {
+    console.error('쿼리 실행 중 오류:', error);
+    setQueryResult({ columns: [], rows: [] });
+  }
+};
--- components/editor/Styles.js
+++ components/editor/Styles.js
@@ -3,16 +3,14 @@ import { EditorView } from 'codemirror';
 
 export const createSqoolTheme = (isDarkMode) => EditorView.theme({
   '&': {
-    color: `${isDarkMode ? '#ffffff' : '#000000'}`,
-    backgroundColor: `${isDarkMode ? '#2d3748' : '#edf2f7'}`,
+    backgroundColor: 'transparent',
     fontSize: '14px',
-    lineHeight: '1.5',
     tabSize: '4',
     whiteSpace: 'pre',
-    wordWrap: 'normal',
-    overflowWrap: 'normal',
+    // wordWrap: 'normal',
+    // overflowWrap: 'normal',
     hyphens: 'none',
-    minHeight: '200px',
+    minHeight: '400px',
   },
   '&.cm-editor': {
     outline: 'none',
@@ -20,13 +18,51 @@ export const createSqoolTheme = (isDarkMode) => EditorView.theme({
   '.cm-content': {
     padding: '8px',
   },
+  '.cm-gutters' : {
+    backgroundColor: 'transparent',
+  },
   '.cm-activeLine' : {
-    backgroundColor: `${isDarkMode ? '#000000' : '#ffffff'}`,
+    backgroundColor: `${isDarkMode ? '#231A2E' : '#F7EBFA'}`,
   },
   '.cm-line': {
+    margin: '0;',
     fontFamily: "'EliceDigitalCodingver.H', monospace",
+  },
+  'span.ͼb': {
+    color: `${isDarkMode ? '#C944EA' : '#A832C7'}`,
   },
   '.cm-gutterElement': {
     fontFamily: "'EliceDigitalCodingver.H', monospace",
+    height: 'auto',
+    textAlign: 'center',
+    padding: '8px 0',
+    marginTop: '0',
   },
+  '.cm-activeLineGutter': {
+    backgroundColor: `${isDarkMode ? '#231A2E' : '#F7EBFA'}`,
+  },
+  '.cm-scroller': {
+    overflowY: 'auto',
+    maxHeight: '400px',
+    scrollbarWidth: 'thin', /* 파이어폭스 */
+    scrollbarColor: '#888 #ccc' /* 파이어폭스 */
+  },
+  /* 웹킷 기반 브라우저 */
+  '.cm-scroller::-webkit-scrollbar': {
+    width: '4px',
+    height: '4px'
+  },
+  '.cm-scroller::-webkit-scrollbar-thumb': {
+    backgroundColor: '#888',
+    borderRadius: '10px'
+  },
+  '.cm-scroller::-webkit-scrollbar-thumb:hover': {
+    backgroundColor: '#555'
+  },
+  '.cm-cursor': {
+    borderLeftColor: isDarkMode ? '#f8fafc' : '#0f172a' // 커서 색상 설정
+  },
+  '&.cm-focused .cm-cursor': {
+    borderLeftColor: isDarkMode ? '#AA55FF' : '#8A2BE2' // 포커스된 상태의 커서 색상 설정
+  }
 });
