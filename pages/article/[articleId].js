745a5a89bab4ddcb540b7bbc4bda3a74ba85efeb
sydneyskim <seung2332@gmail.com>
2024-08-10 00:16:56 +0900

    아티클 데이터 가져오기 수정

--- pages/article/[articleId].js
+++ pages/article/[articleId].js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ArticleDetail from '../../components/article/ArticleDetail';
+import { getArticleDetail } from '../../components/article/Api';
 
 const ArticleDetailPage = () => {
     const router = useRouter();
     const { articleId } = router.query;
-    const article = router.query.article ? JSON.parse(router.query.article) : null;
+    const [article, setArticle] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    if (!article) {
-        return <div>Error loading article</div>;
-    }
+    useEffect(() => {
+        if (articleId) {
+            const fetchArticle = async () => {
+                try {
+                    const data = await getArticleDetail(articleId); // 이 시점에만 조회수 증가
+                    setArticle(data);
+                } catch (error) {
+                    console.error('Error fetching article detail:', error);
+                } finally {
+                    setIsLoading(false);
+                }
+            };
+
+            fetchArticle();
+        }
+    }, [articleId]);
 
     const handleBack = () => {
         router.push('/article');
     };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!article) {
+        return <div>Error loading article</div>;
+    }
 
     return (
         <div className="w-full h-screen flex flex-col items-center p-4" style={{ marginTop: '40px' }}>
