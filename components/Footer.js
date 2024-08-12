66066b39f225ea24e92e0bf39bf0e50f7e5fd952
Baristacus <chic.igel@gmail.com>
2024-08-12 11:35:02 +0900

    Footer Img 경로 수정

--- components/Footer.js
+++ components/Footer.js
@@ -11,18 +11,18 @@ const Footer = () => {
   const container = `w-full py-4 ${isDarkMode ? 'bg-slate-900' : 'bg-slate-200'} text-slate-400 absolute`;
   const footerWrap = `max-w-content-full mx-auto flex justify-between items-center`;
   const iconList = `flex justify-center items-center gap-2`;
-  const linkIcon = `w-8 h-8 p-1 flex justify-center items-center rounded-2xl border-2 ${isDarkMode ? 'border-slate-500' : 'border-slate-300' } hover:opacity-70 ease-in-out duration-150`
+  const linkIcon = `w-8 h-8 p-1 flex justify-center items-center rounded-2xl border-2 ${isDarkMode ? 'border-slate-500' : 'border-slate-300'} hover:opacity-70 ease-in-out duration-150`
 
   return (
     <footer className={container}>
       <div className={footerWrap}>
         <div className='flex items-end gap-4'>
-          <Link href="/" legacyBehavior>  
+          <Link href="/" legacyBehavior>
             <a>
               {isDarkMode ? (
-                <Image src='/Img/ci_logo_mono_dark.svg' alt='Logo' width={117} height={24} className='mr-2' priority />  
+                <Image src='/img/ci_logo_mono_dark.svg' alt='Logo' width={117} height={24} className='mr-2' priority />
               ) : (
-                <Image src='/Img/ci_logo_mono_light.svg' alt='Logo' width={117} height={24} className='mr-2' priority />  
+                <Image src='/img/ci_logo_mono_light.svg' alt='Logo' width={117} height={24} className='mr-2' priority />
               )}
             </a>
           </Link>
@@ -38,20 +38,20 @@ const Footer = () => {
               </Link>
             </li>
             <li>
-                <a className={linkIcon}
-                  href='https://github.com'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                    <LinkGithub width={20} height={20} className="fill-slate-400" title="Github 링크" />
-                </a>
+              <a className={linkIcon}
+                href='https://github.com'
+                target='_blank'
+                rel='noopener noreferrer'>
+                <LinkGithub width={20} height={20} className="fill-slate-400" title="Github 링크" />
+              </a>
             </li>
             <li>
-                <a className={linkIcon}
-                  href='https://discord.com'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  <LinkDiscord width={20} height={20} className="fill-slate-400" title="Discord 링크" />
-                </a>
+              <a className={linkIcon}
+                href='https://discord.com'
+                target='_blank'
+                rel='noopener noreferrer'>
+                <LinkDiscord width={20} height={20} className="fill-slate-400" title="Discord 링크" />
+              </a>
             </li>
           </ul>
         </div>
