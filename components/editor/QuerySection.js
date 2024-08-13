42bfb800a1e0aa7168f98da5ba48b7e664b0a937
Baristacus <chic.igel@gmail.com>
2024-08-13 10:30:03 +0900

    모듈 import 오타 수정

--- components/editor/QuerySection.js
+++ components/editor/QuerySection.js
@@ -7,7 +7,7 @@ import { sql } from "@codemirror/lang-sql";
 import { autocompletion } from "@codemirror/autocomplete";
 import { createSqoolTheme } from "./Styles";
 import { CodeCopy, DBReset } from "../IconSet";
-import { sqliteCompletion } from "./SqliteKeywords";
+import { sqliteCompletion } from "./sqliteKeywords";
 import { keymap } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 import { placeholder } from "@codemirror/view";
